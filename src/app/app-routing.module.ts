8b6f160f117bfd1084f661bda2777dc3fb98d422
Manuel Martinez <manuel.martinez.developer@gmail.com>
2023-12-06 15:44:24 -0400

    user and password

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { PaginaComponent } from './pagina/pagina.component';
 import { ApiComponent } from './api/api.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { LabsComponent } from './labs/labs.component';
+
+import { canActivate, redirectUnauthorizedTo } from '@angular/fire/auth-guard';
 
 const routes: Routes = [
   { path: '', component: PaginaComponent },
-  { path: '', component: ApiComponent}
+  { path: '', component: ApiComponent},
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent},
+  { path: "labs", component:LabsComponent, ...canActivate(()=>redirectUnauthorizedTo(["/"]))}
 ];
 
 @NgModule({
